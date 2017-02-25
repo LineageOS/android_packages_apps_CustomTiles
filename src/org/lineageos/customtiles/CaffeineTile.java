84d53bcadb1313f938c0d1a379231ab2ff2d1530
Luca Stefani <luca020400@lineageos.org>
2017-02-25 16:23:08 +0100

    CustomTiles: Caffeine: Move to a service based tile
    * Based off @otaconix work

--- src/org/lineageos/customtiles/CaffeineTile.java
+++ src/org/lineageos/customtiles/CaffeineTile.java
@@ -16,159 +16,71 @@
 package org.lineageos.customtiles;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.graphics.drawable.Icon;
-import android.os.CountDownTimer;
-import android.os.PowerManager;
-import android.os.SystemClock;
+import android.os.IBinder;
+import android.os.UserHandle;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
 public class CaffeineTile extends TileService {
-
-    private PowerManager.WakeLock mWakeLock;
-    private int mSecondsRemaining;
-    private int mDuration;
-    private static int[] DURATIONS = new int[] {
-        5 * 60,   // 5 min
-        10 * 60,  // 10 min
-        30 * 60,  // 30 min
-        -1,       // infinity
-    };
-    private CountDownTimer mCountdownTimer = null;
-    public long mLastClickTime = -1;
+    private WakelockService wakelockService;
+    private ServiceConnection serviceConnection;
 
     @Override
     public void onStartListening() {
         super.onStartListening();
 
-        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
-        mWakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK,
-                "CaffeineTile");
+        startService(new Intent(getApplicationContext(), WakelockService.class));
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                wakelockService = ((WakelockService.Binder) iBinder).getService();
+                refresh();
+            }
 
-        final IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        registerReceiver(mReceiver, filter);
-
-        refresh();
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                wakelockService = null;
+                refresh();
+            }
+        };
+        bindServiceAsUser(new Intent(getApplicationContext(), WakelockService.class),
+                serviceConnection, 0, UserHandle.CURRENT);
     }
 
     @Override
     public void onStopListening() {
-        super.onStopListening();
+        unbindService(serviceConnection);
+    }
 
-        stopCountDown();
-        unregisterReceiver(mReceiver);
-        if (mWakeLock.isHeld())
-            mWakeLock.release();
+    @Override
+    public void onTileRemoved() {
+        stopService(new Intent(getApplicationContext(), WakelockService.class));
     }
 
     @Override
     public void onClick() {
         super.onClick();
 
-        // If last user clicks < 5 seconds
-        // we cycle different duration
-        // otherwise toggle on/off
-        if (mWakeLock.isHeld() && (mLastClickTime != -1) &&
-                (SystemClock.elapsedRealtime() - mLastClickTime < 5000)) {
-            // cycle duration
-            mDuration++;
-            if (mDuration >= DURATIONS.length) {
-                // all durations cycled, turn if off
-                mDuration = -1;
-                stopCountDown();
-                if (mWakeLock.isHeld()) {
-                    mWakeLock.release();
-                }
-            } else {
-                // change duration
-                startCountDown(DURATIONS[mDuration]);
-                if (!mWakeLock.isHeld()) {
-                    mWakeLock.acquire();
-                }
-            }
-        } else {
-            // toggle
-            if (mWakeLock.isHeld()) {
-                mWakeLock.release();
-                stopCountDown();
-            } else {
-                mWakeLock.acquire();
-                mDuration = 0;
-                startCountDown(DURATIONS[mDuration]);
-            }
-        }
-        mLastClickTime = SystemClock.elapsedRealtime();
+        if (wakelockService != null)
+            wakelockService.toggle();
 
         refresh();
     }
 
-    private void startCountDown(int duration) {
-        stopCountDown();
-        mSecondsRemaining = duration;
-        if (duration == -1) {
-            // infinity timing, no need to start timer
-            return;
-        }
-
-        mCountdownTimer = new CountDownTimer(duration * 1000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mSecondsRemaining = (int) (millisUntilFinished / 1000);
-                refresh();
-            }
-
-            @Override
-            public void onFinish() {
-                if (mWakeLock.isHeld())
-                    mWakeLock.release();
-                refresh();
-            }
-
-        }.start();
-    }
-
-    private void stopCountDown() {
-        if (mCountdownTimer != null) {
-            mCountdownTimer.cancel();
-            mCountdownTimer = null;
-        }
-    }
-
-    private String formatValueWithRemainingTime() {
-        if (mSecondsRemaining == -1) {
-            return "\u221E"; // infinity
-        }
-        return String.format("%02d:%02d",
-                        mSecondsRemaining / 60 % 60, mSecondsRemaining % 60);
-    }
-
     private void refresh() {
-        if (mWakeLock.isHeld()) {
-            getQsTile().setLabel(formatValueWithRemainingTime());
+        getQsTile().setLabel(getString(R.string.caffeine_label));
+        if (wakelockService != null && wakelockService.isActive()) {
             getQsTile().setIcon(Icon.createWithResource(this, R.drawable.ic_caffeine_on));
             getQsTile().setState(Tile.STATE_ACTIVE);
         } else {
-            getQsTile().setLabel(getString(R.string.caffeine_label));
             getQsTile().setIcon(Icon.createWithResource(this, R.drawable.ic_caffeine_off));
             getQsTile().setState(Tile.STATE_INACTIVE);
         }
         getQsTile().updateTile();
     }
-
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
-                // disable caffeine if user force off (power button)
-                stopCountDown();
-                if (mWakeLock.isHeld())
-                    mWakeLock.release();
-                refresh();
-            }
-        }
-    };
 }
---
+++ src/org/lineageos/customtiles/WakelockService.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.customtiles;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.preference.PreferenceManager;
+
+public class WakelockService extends Service {
+    private PowerManager.WakeLock wakeLock;
+    private ScreenOffReceiver screenOffReceiver = new ScreenOffReceiver();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        wakeLock = createWakeLock();
+        screenOffReceiver.init();
+    }
+
+    private PowerManager.WakeLock createWakeLock() {
+        final int wakeLockType = PowerManager.SCREEN_BRIGHT_WAKE_LOCK;
+
+        PowerManager.WakeLock newWakeLock = getSystemService(PowerManager.class).newWakeLock
+                (wakeLockType, "Caffeine tile");
+        newWakeLock.setReferenceCounted(false);
+
+        return newWakeLock;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (wakeLock.isHeld())
+            wakeLock.release();
+
+        screenOffReceiver.destroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new Binder();
+    }
+
+    public boolean isActive() {
+        return wakeLock.isHeld();
+    }
+
+    public void toggle() {
+        if (wakeLock.isHeld())
+            wakeLock.release();
+        else
+            wakeLock.acquire();
+    }
+
+    public class Binder extends android.os.Binder {
+        public WakelockService getService() {
+            return WakelockService.this;
+        }
+    }
+
+    private class ScreenOffReceiver extends BroadcastReceiver {
+        public void init() {
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            getApplicationContext().registerReceiver(this, filter);
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+                if (wakeLock.isHeld())
+                    toggle();
+            }
+        }
+
+        public void destroy() {
+            getApplicationContext().unregisterReceiver(this);
+        }
+    }
+}
